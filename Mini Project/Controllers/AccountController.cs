74a3d6086674c596fd3202ca6817299e1ec6a4b0
saeedkp <kp_saeed1@yahoo.com>
2021-09-06 02:19:31 +0430

    Complete Trainee Registration

--- Mini Project/Controllers/AccountController.cs
+++ Mini Project/Controllers/AccountController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Mini_Project.Models;
+using Mini_Project.Services;
 using Mini_Project.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,12 +18,24 @@ namespace Mini_Project.Controllers
     {
         private readonly UserManager<ApplicationUser> userManager;
         private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly IMailService mailService;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IWebHostEnvironment hostingEnvironment;
+        private readonly IRequestRepository _requestRepository;
 
         public AccountController(UserManager<ApplicationUser> userManager,
-                                SignInManager<ApplicationUser> signInManager)
+                                SignInManager<ApplicationUser> signInManager,
+                                IMailService mailService,
+                                IRequestRepository requestRepository,
+                                RoleManager<IdentityRole> roleManager,
+                                IWebHostEnvironment hostingEnvironment)
         {
             this.signInManager = signInManager;
+            this.mailService = mailService;
+            this.roleManager = roleManager;
+            this.hostingEnvironment = hostingEnvironment;
             this.userManager = userManager;
+            _requestRepository = requestRepository;
         }
 
         [HttpPost]
@@ -80,5 +95,63 @@ namespace Mini_Project.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Request request = _requestRepository.GetRequestByEmail(model.Email);
+
+                string UniqueFileName = ProcessUploadedFile(model);
+
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    firstName = request.firstName,
+                    lastName = request.lastName,
+                    documentsPath = UniqueFileName
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                var roleResult = await userManager.AddToRoleAsync(user, "Trainee");
+
+                if (result.Succeeded || roleResult.Succeeded)
+                {
+                    request.state = State.WaitingForDocumentAcception;
+                    _requestRepository.Update(request);
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("createrequest", "home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+        private string ProcessUploadedFile(RegisterViewModel model)
+        {
+            string UniqueFileName = null;
+            if (model.Documents != null)
+            {
+                string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "documents");
+                UniqueFileName = Guid.NewGuid().ToString() + "_" + model.Documents.FileName;
+                string filePath = Path.Combine(uploadsFolder, UniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Documents.CopyTo(fileStream);
+                }
+            }
+
+            return UniqueFileName;
+        }
+
     }
 }
